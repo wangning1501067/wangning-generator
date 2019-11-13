2cdb62f37792f004933fbf4cfc6520320a351668
wangning <a>
2019-11-13 09:42:46 +0800

    随便

---
+++ src/main/java/com/ecps/seckill/pojo/Employee.java
@@ -0,0 +1,375 @@
+package com.ecps.seckill.pojo;
+
+import java.util.Date;
+
+public class Employee {
+    private Integer employeeId;
+
+    private String employeeZhName;
+
+    private String employeeCode;
+
+    private String employeeCnName;
+
+    private String employeePassword;
+
+    private Short sex;
+
+    private String mobilePhone;
+
+    private Short workeStatus;
+
+    private Short employeeType;
+
+    private Date dutyDate;
+
+    private String email;
+
+    private String organization;
+
+    private String department;
+
+    private String officeLocation;
+
+    private Integer post;
+
+    private String appPassword;
+
+    private Short collectionStatus;
+
+    private Short authenticationStatus;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Short ifDelete;
+
+    private Short authorizedState;
+
+    private String imagePath;
+
+    private Integer roleId;
+
+    private Integer createUser;
+
+    private Integer updateUser;
+
+    private Short identityFlag;
+
+    private Short accountPassword;
+
+    private Short active;
+
+    private Integer appId;
+
+    private String workAreaStr;
+
+    private Long integral;
+
+    private Date integralUpdateTime;
+
+    private String headPath;
+
+    private Short employeeStatus;
+
+    private String skill;
+
+    private String faceAuthorImage;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeZhName() {
+        return employeeZhName;
+    }
+
+    public void setEmployeeZhName(String employeeZhName) {
+        this.employeeZhName = employeeZhName == null ? null : employeeZhName.trim();
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode == null ? null : employeeCode.trim();
+    }
+
+    public String getEmployeeCnName() {
+        return employeeCnName;
+    }
+
+    public void setEmployeeCnName(String employeeCnName) {
+        this.employeeCnName = employeeCnName == null ? null : employeeCnName.trim();
+    }
+
+    public String getEmployeePassword() {
+        return employeePassword;
+    }
+
+    public void setEmployeePassword(String employeePassword) {
+        this.employeePassword = employeePassword == null ? null : employeePassword.trim();
+    }
+
+    public Short getSex() {
+        return sex;
+    }
+
+    public void setSex(Short sex) {
+        this.sex = sex;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone == null ? null : mobilePhone.trim();
+    }
+
+    public Short getWorkeStatus() {
+        return workeStatus;
+    }
+
+    public void setWorkeStatus(Short workeStatus) {
+        this.workeStatus = workeStatus;
+    }
+
+    public Short getEmployeeType() {
+        return employeeType;
+    }
+
+    public void setEmployeeType(Short employeeType) {
+        this.employeeType = employeeType;
+    }
+
+    public Date getDutyDate() {
+        return dutyDate;
+    }
+
+    public void setDutyDate(Date dutyDate) {
+        this.dutyDate = dutyDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization == null ? null : organization.trim();
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department == null ? null : department.trim();
+    }
+
+    public String getOfficeLocation() {
+        return officeLocation;
+    }
+
+    public void setOfficeLocation(String officeLocation) {
+        this.officeLocation = officeLocation == null ? null : officeLocation.trim();
+    }
+
+    public Integer getPost() {
+        return post;
+    }
+
+    public void setPost(Integer post) {
+        this.post = post;
+    }
+
+    public String getAppPassword() {
+        return appPassword;
+    }
+
+    public void setAppPassword(String appPassword) {
+        this.appPassword = appPassword == null ? null : appPassword.trim();
+    }
+
+    public Short getCollectionStatus() {
+        return collectionStatus;
+    }
+
+    public void setCollectionStatus(Short collectionStatus) {
+        this.collectionStatus = collectionStatus;
+    }
+
+    public Short getAuthenticationStatus() {
+        return authenticationStatus;
+    }
+
+    public void setAuthenticationStatus(Short authenticationStatus) {
+        this.authenticationStatus = authenticationStatus;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Short getIfDelete() {
+        return ifDelete;
+    }
+
+    public void setIfDelete(Short ifDelete) {
+        this.ifDelete = ifDelete;
+    }
+
+    public Short getAuthorizedState() {
+        return authorizedState;
+    }
+
+    public void setAuthorizedState(Short authorizedState) {
+        this.authorizedState = authorizedState;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath == null ? null : imagePath.trim();
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(Integer createUser) {
+        this.createUser = createUser;
+    }
+
+    public Integer getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(Integer updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Short getIdentityFlag() {
+        return identityFlag;
+    }
+
+    public void setIdentityFlag(Short identityFlag) {
+        this.identityFlag = identityFlag;
+    }
+
+    public Short getAccountPassword() {
+        return accountPassword;
+    }
+
+    public void setAccountPassword(Short accountPassword) {
+        this.accountPassword = accountPassword;
+    }
+
+    public Short getActive() {
+        return active;
+    }
+
+    public void setActive(Short active) {
+        this.active = active;
+    }
+
+    public Integer getAppId() {
+        return appId;
+    }
+
+    public void setAppId(Integer appId) {
+        this.appId = appId;
+    }
+
+    public String getWorkAreaStr() {
+        return workAreaStr;
+    }
+
+    public void setWorkAreaStr(String workAreaStr) {
+        this.workAreaStr = workAreaStr == null ? null : workAreaStr.trim();
+    }
+
+    public Long getIntegral() {
+        return integral;
+    }
+
+    public void setIntegral(Long integral) {
+        this.integral = integral;
+    }
+
+    public Date getIntegralUpdateTime() {
+        return integralUpdateTime;
+    }
+
+    public void setIntegralUpdateTime(Date integralUpdateTime) {
+        this.integralUpdateTime = integralUpdateTime;
+    }
+
+    public String getHeadPath() {
+        return headPath;
+    }
+
+    public void setHeadPath(String headPath) {
+        this.headPath = headPath == null ? null : headPath.trim();
+    }
+
+    public Short getEmployeeStatus() {
+        return employeeStatus;
+    }
+
+    public void setEmployeeStatus(Short employeeStatus) {
+        this.employeeStatus = employeeStatus;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill == null ? null : skill.trim();
+    }
+
+    public String getFaceAuthorImage() {
+        return faceAuthorImage;
+    }
+
+    public void setFaceAuthorImage(String faceAuthorImage) {
+        this.faceAuthorImage = faceAuthorImage == null ? null : faceAuthorImage.trim();
+    }
+}
